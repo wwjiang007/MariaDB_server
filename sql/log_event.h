9d04b702a852e5ab9ce5f43e07de19da330615b2
Nikita Malyavin <nikitamalyavin@gmail.com>
2022-11-02 14:19:12 +0300

    MDEV-29069 follow-up optimize find_key
    1. Avoid traversion of all key_parts for all keys for every event.
    The key is enough to be determined only once per table open, so
    RPL_TABLE_LIST can be used to store designated key.
    We suppose here that no key can be deleted on the replica node.
    The algorithm "choose PRIMARY KEY first, then if none, any other key" will
    work for any ROW_FORMAT.
    Proof:
    For FULL any key is good. If PK is available, it'll be chosen.
    For MINIMAL PK is always sent. If there's no PK, then a full image is sent.
    So either it has PK, and it's chosen, or ROW_FORMAT fallbacks to FULL.
    NOBLOB works same as MINIMAL.
    
    2. Move optimization for avoiding m_key allocation inside find_key,
    so now it is used by UPDATE events too.
    
    3. Together with unpack_row, do_before_row_operations is now same for
    Update_rows_log_event and Delete_rows_log_event, except for a few bits.
    It's all moved to a common function
    Rows_log_event::do_before_lookup_row_operations

--- sql/log_event.h
+++ sql/log_event.h
@@ -4993,6 +4993,7 @@ private:
   unsigned char *m_optional_metadata;
 };
 
+struct Rpl_table_data;
 
 /**
   @class Rows_log_event
@@ -5265,7 +5266,8 @@ protected:
   uint      m_usable_key_parts; /* A number of key_parts suited to lookup */
   bool master_had_triggers;     /* set after tables opening */
 
-  int find_key(); // Find a best key to use in find_row()
+  /** Find a best key to use in find_row() */
+  int find_key(Rpl_table_data *table_data);
   int find_row(rpl_group_info *);
   int write_row(rpl_group_info *, const bool);
   int update_sequence();
@@ -5306,6 +5308,10 @@ protected:
     return (m_table->next_number_field &&
             m_table->next_number_field->field_index >= m_width);
   }
+
+  /** before_row_operations part common for delete and update events */
+  int do_before_lookup_row_operations(const rpl_group_info *rgi,
+                                      Rpl_table_data *table_data);
 #endif
 
 private:
@@ -5331,8 +5337,9 @@ private:
       The member function will return 0 if all went OK, or a non-zero
       error code otherwise.
   */
-  virtual 
-  int do_before_row_operations(const rpl_group_info *rgi) = 0;
+  virtual
+  int do_before_row_operations(const rpl_group_info *rgi,
+                               Rpl_table_data *table_data) = 0;
 
   /*
     Primitive to clean up after a sequence of row executions.
@@ -5423,7 +5430,8 @@ private:
 #endif
 
 #if defined(MYSQL_SERVER) && defined(HAVE_REPLICATION)
-  virtual int do_before_row_operations(const rpl_group_info *);
+  virtual int
+  do_before_row_operations(const rpl_group_info *, Rpl_table_data *table_data);
   virtual int do_after_row_operations(const Slave_reporting_capability *const,int);
   virtual int do_exec_row(rpl_group_info *);
 #endif
@@ -5513,7 +5521,8 @@ protected:
 #endif
 
 #if defined(MYSQL_SERVER) && defined(HAVE_REPLICATION)
-  virtual int do_before_row_operations(const rpl_group_info *rgi);
+  virtual int do_before_row_operations(const rpl_group_info *rgi,
+                                       Rpl_table_data *table_data);
   virtual int do_after_row_operations(const Slave_reporting_capability *const,int);
   virtual int do_exec_row(rpl_group_info *);
 #endif /* defined(MYSQL_SERVER) && defined(HAVE_REPLICATION) */
@@ -5600,7 +5609,8 @@ protected:
 #endif
 
 #if defined(MYSQL_SERVER) && defined(HAVE_REPLICATION)
-  virtual int do_before_row_operations(const rpl_group_info *rgi);
+  virtual int do_before_row_operations(const rpl_group_info *rgi,
+                                       Rpl_table_data *table_data);
   virtual int do_after_row_operations(const Slave_reporting_capability *const,int);
   virtual int do_exec_row(rpl_group_info *);
 #endif
