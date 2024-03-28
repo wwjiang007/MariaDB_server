MariaDB/server

MDEV-25829 Change default Unicode collation to uca1400_ai_ci
Step#3 The main patch

sql/item_strfunc.h
@@ -2016,7 +2016,7 @@ public:
   Item_func_expr_str_metadata(THD *thd, Item *a): Item_str_func(thd, a) { }
   bool fix_length_and_dec(THD *thd) override
   {
-     collation.set(system_charset_info_for_i_s);
+     collation.set(system_charset_info_for_i_s, DERIVATION_SYSCONST);
      max_length= 64 * collation.collation->mbmaxlen; // should be enough
      base_flags&= ~item_base_t::MAYBE_NULL;
      return FALSE;

wwjiang007/MariaDB_server

sql/item_strfunc.h
@@ -2016,7 +2016,7 @@ public:
   Item_func_expr_str_metadata(THD *thd, Item *a): Item_str_func(thd, a) { }
   bool fix_length_and_dec(THD *thd) override
   {
-     collation.set(system_charset_info);
+     collation.set(system_charset_info, DERIVATION_SYSCONST);
      max_length= 64 * collation.collation->mbmaxlen; // should be enough
      base_flags&= ~item_base_t::MAYBE_NULL;
      return FALSE;