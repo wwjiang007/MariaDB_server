58838f57fbb1e19d965bee5b0582c94f1ecdaa2b
Rex <rex.johnston@mariadb.com>
2024-04-03 12:55:24 +1200

    MDEV-28621 group by optimization incorrectly removing subquery where subject buried in a function
    A group by query optimization technique, where the a group by clause in
    a subquery can be removed is incorrectly removing references to subquery
    items that may be defined and used elsewhere.
    
    This is the initial implementation of a reference counter in the unit
    attached to an Item_subquery.

--- sql/item.cc
+++ sql/item.cc
@@ -6085,6 +6085,9 @@ bool Item_field::fix_fields(THD *thd, Item **reference)
                                   select->context_analysis_place == IN_GROUP_BY && 
 				  alias_name_used  ?  *rf->ref : rf);
 
+            if ((*res)->type() == Item::SUBSELECT_ITEM)
+              ((Item_subselect*)(*res))->reference_count++;
+
             /*
               We can not "move" aggregate function in the place where
               its arguments are not defined.
