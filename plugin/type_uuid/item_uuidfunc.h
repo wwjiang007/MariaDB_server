StefanoPetrilli/server

MDEV-11339 Implement native UUID4 function

plugin/type_uuid/item_uuidfunc.h
@@ -64,6 +64,37 @@ public:
   { return get_item_copy<Item_func_uuid>(thd, this); }
 };
 
+class Item_func_uuid_v4: public Item_str_func
+{
+protected:
+  size_t uuid_len() const
+  { return MY_UUID_BARE_STRING_LENGTH + MY_UUID_SEPARATORS; }
+public:
+  Item_func_uuid_v4(THD *thd): Item_str_func(thd) {}
+  const Type_handler *type_handler() const override;
+  bool fix_length_and_dec(THD *thd) override
+  {
+    collation.set(DTCollation_numeric());
+    fix_char_length(uuid_len());
+    return FALSE;
+  }
+  bool const_item() const override { return false; }
+  table_map used_tables() const override { return RAND_TABLE_BIT; }
+  LEX_CSTRING func_name_cstring() const override
+  {
+    static LEX_CSTRING name= {STRING_WITH_LEN("uuidv4") };
+    return name;
+  }
+  String *val_str(String *) override;
+  bool val_native(THD *thd, Native *to) override;
+  bool check_vcol_func_processor(void *arg) override
+  {
+    return mark_unsupported_function(func_name(), "()", arg, VCOL_NON_DETERMINISTIC);
+  }
+  Item *get_copy(THD *thd) override
+  { return get_item_copy<Item_func_uuid_v4>(thd, this); }
+};
+
 class Item_func_uuid_v7: public Item_str_func
 {
 protected:

wwjiang007/MariaDB_server

plugin/type_uuid/item_uuidfunc.h
@@ -78,4 +78,19 @@ public:
   { return get_item_copy<Item_func_uuid>(thd, this); }
 };
 
+class Item_func_uuid_v4: public Item_func_uuid
+{
+public:
+  Item_func_uuid_v4(THD *thd): Item_func_uuid(thd) { }
+  LEX_CSTRING func_name_cstring() const override
+  {
+    static LEX_CSTRING name= {STRING_WITH_LEN("uuidv4") };
+    return name;
+  }
+  String *val_str(String *) override;
+  bool val_native(THD *thd, Native *to) override;
+  Item *get_copy(THD *thd) override
+  { return get_item_copy<Item_func_uuid_v4>(thd, this); }
+};
+
 #endif // ITEM_UUIDFUNC_INCLUDED